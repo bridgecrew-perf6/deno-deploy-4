479989f7efa77154abb732910dfb37abacbadf02
Myles O'Connor <myles@oconnor.industries>
2022-03-23 12:36:29 -0700

    Read env var

--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
 import { ConnInfo, serve } from 'https://deno.land/std@0.114.0/http/server.ts'
 
+const author = Deno.env.get("AUTHOR")!
+
 function handler(_req: Request, connInfo: ConnInfo) {
   const addr = connInfo.remoteAddr as Deno.NetAddr
   const ip = addr.hostname
 
-  return new Response(`Your IP address is <b>${ip}</b>`, {
+  return new Response(`<p style='font-family: monospace'>Your IP address is <b>${ip}</b>.<br/>-${author}</p>`, {
     headers: { "content-type": "text/html" },
   })
 }
